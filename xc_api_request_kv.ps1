a6a7d519fd8629a9abe13eb663fc5b0584067da7
stockerts <108150421+stockerts@users.noreply.github.com>
2023-02-27 10:55:23 -0800

    Updated format

--- xc_api_request_kv.ps1
+++ xc_api_request_kv.ps1
@@ -19,8 +19,8 @@ $secret = Get-AzKeyVaultSecret -VaultName $azkvname -Name $azkvsecretname -AsPla
 
 # Define the header variable
 $header = @{
-    'Content-Type' = $content
-    'Authorization' = $secret
+  'Content-Type' = $content
+  'Authorization' = $secret
 }
 
 # Get the content of the JSON file
@@ -28,19 +28,19 @@ $body = Get-Content ".\$path\$filename.json"
 
 # Get the asset name from the JSON file
 if ('active_service_policies' -ine $operation) {
-$assetname = $body | ConvertFrom-Json |
-    Select-Object -ExpandProperty metadata |
-    Select-Object -ExpandProperty name
+  $assetname = $body | ConvertFrom-Json |
+  Select-Object -ExpandProperty metadata |
+  Select-Object -ExpandProperty name
 }
     
 # Get the request status
 if ('active_service_policies' -ine $operation) {
-Write-Verbose -Verbose "Checking for $operation named $assetname"
-Try {
-  Invoke-WebRequest -Uri "https://$domain/api/config/namespaces/$namespace/$operation/$assetname" -Method get -Header $header
-} Catch {
-  $status = $_.Exception.Response.StatusCode
-}
+  Write-Verbose -Verbose "Checking for $operation named $assetname"
+  Try {
+    Invoke-WebRequest -Uri "https://$domain/api/config/namespaces/$namespace/$operation/$assetname" -Method get -Header $header
+  } Catch {
+    $status = $_.Exception.Response.StatusCode
+  }
 }
 
 # Create or update the asset based on the request status
